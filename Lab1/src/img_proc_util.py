6c6bdb55720e701b29c0b00d986a29d81e0624a9
ExRamic <eramicc@gmail.com>
2022-05-24 09:34:31 +0200

    Updated

--- Lab1/src/img_proc_util.py
+++ Lab1/src/img_proc_util.py
@@ -4,9 +4,10 @@
 This module contains custom fits functions
 """
 
-from typing import Dict
 import numpy as np
+import cv2
 from astropy.io import fits
+import matplotlib.pyplot as plt
 
 
 # returns 3x3 transformation matrix
@@ -27,9 +28,32 @@ def trans_parameter(hdu_primary: fits.PrimaryHDU) -> np.ndarray:
             [val6, val7, val8]  ])
 
 
-def image_timestamp_list(hdul: fits.HDUList) -> list[int, np.ndarray]:
+def image_timestamp_list(hdul: fits.HDUList):
     list = []
     for i in range(1, len(hdul)):
         timestamp = int(hdul[i].header.cards["TIMESTMP"].value)
         list.append([timestamp, hdul[i].data])  
     return list
+
+
+def prepare_color_data(image_time_list):
+    list = []
+    for i in range(len(image_time_list)):
+        image = cv2.cvtColor(image_time_list[i][1].astype(np.uint8), cv2.COLOR_BAYER_BG2RGB)
+        list.append([image_time_list[i][0], image])
+    return list
+
+def prepare_fluo_data(image_time_list):
+    list = []
+    for i in range(len(image_time_list)):
+        image = cv2.resize(image_time_list[i][1], (1392, 1024))
+        list.append([image_time_list[i][0], image])
+    return list
+
+def display_image(image):
+    #plt.subplot()
+    plt.imshow(image)
+    plt.show()
+
+
+
--- Lab1/src/optical_imaging.py
+++ Lab1/src/optical_imaging.py
@@ -3,12 +3,8 @@
 """
 This module contains the OpticalImaging Class
 """
-import numpy as np
-import matplotlib.pyplot as plt
-import cv2
-from astropy.io import fits
 
-import img_proc_util
+from img_proc_util import *
 
 
 class OpticalImaging:
@@ -39,89 +35,28 @@ class OpticalImaging:
             print("Error while trying to read color image fits file!")
 
         # contains list: [[timestamp1, image_data1], [timestamp2, image_data2], ... ]
-        self.color_images_time_list = img_proc_util.image_timestamp_list(self.color_data)
-        self.fluo_images_time_list = img_proc_util.image_timestamp_list(self.fluo_data)
+        self.color_images_time_list = image_timestamp_list(self.color_data)
+        self.fluo_images_time_list = image_timestamp_list(self.fluo_data)
 
         # contains 3x3 transformations matrix
-        self.fluo_trans_3x3matrix = img_proc_util.trans_parameter(self.fluo_data[0])
+        self.fluo_trans_3x3matrix = trans_parameter(self.fluo_data[0])
+        #print(self.fluo_trans_3x3matrix)
 
         self.color_data.close()
         self.fluo_data.close()
 
-        print(self.fluo_trans_3x3matrix)
+        #TODO: find correspondings depending on timestamps
 
-        #print(self.color_images_time_list)
-        #print(self.fluo_images_time_list)
-#        self.prepare_color_data()
-#        self.prepare_fluo_data()
-#
-#        # The cameras have different resolution, size and format. Co-Registrate the images!
-#        self.coregistration_images()
-#
-#        self.display_color_images()
-#        self.display_fluo_images()
-#
-#    def prepare_color_data(self):
-#        """
-#        This method takes the color images, change the data type and transform the images into rgb images.
-#        """
-#        # data type has to be uint16 for every image
-#        for image in self.color_images_data:
-#            image = np.uint16(image)
-#            # Convert
-#            self.color_images_rgb.append(cv2.cvtColor(image, cv2.COLOR_BAYER_BG2RGB))
-#
-#        print("1. Color Image Data: nDim >>> ", self.color_images_data[0].ndim)
-#        print("1. Color Image Data: shape >>> ", self.color_images_data[0].shape)
-#        print("1. Color Image Data: size >>> ", self.color_images_data[0].size)
-#        print("1. Color Image Data: 1. pixel >>> ", self.color_images_data[0][0][0])
-#        #print(self.color_images_data[0])
-#
-#    def prepare_fluo_data(self):
-#        """
-#        This method takes the fluo images, ...
-#        """
-#        for image in self.fluo_images_data:  # TODO
-#            image = np.uint16(image)
-#            # Convert
-#            image = cv2.resize(image, (1392, 1024))
-#            #self.fluo_images_rgb.append(cv2.cvtColor(image, cv2.COLOR_BAYER_BG2RGB))
-#            # image = cv2.rotate(image, cv2.ROTATE_180)
-#            #rgb_weights = [0.2989, 0.5870, 0.1140]
-#            # np.dot(image[:, :, 3], rgb_weights)
-#            self.fluo_images_rgb.append(image)
-#
-#    def display_color_images(self):
-#        """
-#        This method plots the color images.
-#        """
-#
-#        print("1. Color Image: nDim >>> ", self.color_images_rgb[0].ndim)
-#        print("1. Color Image: shape >>> ", self.color_images_rgb[0].shape)
-#        print("1. Color Image: size >>> ", self.color_images_rgb[0].size)
-#        print("1. Color Image: 1. pixel >>> ", self.color_images_rgb[0][0][0])
-#        #print(self.color_images_rgb[0])        
-#
-#        plt.imshow(self.color_images_rgb[0])
-#        plt.show()
-#
-#    def display_fluo_images(self):
-#        """
-#        This method plots the color images.
-#        """
-#        print("1. Fluo Image: nDim >>> ", self.fluo_images_rgb[0].ndim)
-#        print("1. Fluo Image: shape >>> ", self.fluo_images_rgb[0].shape)
-#        print("1. Fluo Image: size >>> ", self.fluo_images_rgb[0].size)
-#        print("1. Fluo Fluo Image: 1. pixel >>> ", self.fluo_images_rgb[0][0][0])
-#        #print(self.fluo_images_rgb[0])
-#        # print("1. Image: size >>> ", self.fluo_images_rgb[0].size)
-#
-#        #self.fluo_images_rgb[0] -= 300
-#
-#        # Wenn man sich das Bild unten auf der PDF anschaut, ist blau grün iwie aber schon richtig :D
-#        #plt.imshow(self.fluo_images_rgb[0], cmap="Greys")
-#        plt.imshow(self.fluo_images_rgb[0])
-#        plt.show()
+        self.color_images_time_list = prepare_color_data(self.color_images_time_list)
+        self.fluo_images_time_list = prepare_fluo_data(self.fluo_images_time_list)
+
+        display_image(self.color_images_time_list[0][1])
+        display_image(self.fluo_images_time_list[0][1])
+
+        #TODO: cv2.warpPerspective with 3x3 matrix
+        # The cameras have different resolution, size and format. Co-Registrate the images!       
+        # self.coregistration_images()
+
 #
 #    def coregistration_images(self):
 #        pass
