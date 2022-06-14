152a38388a92a3fbd13fde20571d0f06c1716f96
ExRamic <eramicc@gmail.com>
2022-06-14 21:31:55 +0200

    etzte Aufgabe hinzugefuegt

--- Lab1/src/img_proc_util.py
+++ Lab1/src/img_proc_util.py
@@ -32,7 +32,7 @@ def trans_parameter(hdu_primary: fits.PrimaryHDU) -> np.ndarray:
 def image_timestamp_list(hdul: fits.HDUList):
     list = []
     for i in range(1, len(hdul)):
-    #for i in range(1, 600):
+    #for i in range(1, 100):
         timestamp = int(hdul[i].header.cards["TIMESTMP"].value)
         list.append([timestamp, hdul[i].data])
     return list
@@ -73,7 +73,8 @@ def prepare_fluo_data(image_time_list: 'list[list[int]]', trans_matrix: np.ndarr
         image = cv2.warpPerspective(image, trans_matrix, (1392, 1024))
         image = cv2.threshold(image, 2200, 2500, cv2.THRESH_BINARY)[1]
         image = cv2.erode(image, kernel = np.ones((5, 5), np.uint8), iterations=4)
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB).astype(np.uint8)
+        #image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB).astype(np.uint8)
+        image = cv2.applyColorMap(cv2.cvtColor(image, cv2.COLOR_GRAY2RGB).astype(np.uint8), cv2.COLORMAP_OCEAN)
         #image = cv2.convertScaleAbs(image, alpha=(255.0/65535.0)) # -> converts to uint8
         #image = cv2.threshold(image, 11, 15, cv2.THRESH_BINARY)[1]
         #image = cv2.cvtColor(image, cv2.COLOR_GRAY2RGB)
@@ -96,9 +97,9 @@ def display_image(color_image: np.ndarray):
     plt.imshow(color_image)
     plt.show()
 
-def generate_video(overlapped_list: 'list[np.ndarray]'):
+def generate_video(overlapped_list: 'list[np.ndarray]', name, shape):
     fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
-    video = cv2.VideoWriter('video.mp4',fourcc, 20, (1392,1024))
+    video = cv2.VideoWriter(name, fourcc, 20, shape)
     for i in overlapped_list:
         video.write(i)  
     video.release()
@@ -108,6 +109,94 @@ def overlap(color_list: 'list[list[int]]', fluo_list: 'list[list[int]]', all_tim
     overlapped = []
     for i in range(800):
     #for i in range(N_iterations):
-        image = cv2.addWeighted(cv2.cvtColor(color_list[all_times[i][0]][1], cv2.COLOR_BGR2RGB), 1.0, fluo_list[all_times[i][1]][1], 0.3, 0)
+        image = cv2.addWeighted(cv2.cvtColor(color_list[all_times[i][0]][1], cv2.COLOR_BGR2RGB), 1, fluo_list[all_times[i][1]][1], 0.3, 0)
         overlapped.append(image)
     return overlapped
+
+
+#------------------------------------------------------------------------------------------------------------------
+# Aufgabe 4: hochaufgeloestes ueberlagertes Falschfarbenfilb
+
+def capture_video(video_path):
+    images = []
+    video_file = cv2.VideoCapture(video_path)
+
+    if (video_file.isOpened() == False):
+        print("Error opening video stream or file")
+
+    while (video_file.isOpened()):
+        ret, frame = video_file.read()
+        if ret == True:
+            images.append(frame)
+        else:
+            break
+
+    video_file.release()
+    cv2.destroyAllWindows()
+    return images
+
+def prepare_fluo_data_2(image_list):
+    list = []
+    for i in range(len(image_list)):
+        image = cv2.resize(image_list[i], (768, 576))
+        image = cv2.flip(image, 1)
+        image = cv2.threshold(image, 2200, 2500, cv2.THRESH_BINARY)[1]
+        image = cv2.erode(image, kernel=np.ones((5, 5)), iterations=4)
+        image = cv2.dilate(image, kernel=np.ones((5, 5)), iterations=4)
+        image = cv2.applyColorMap(cv2.cvtColor(image, cv2.COLOR_GRAY2RGB).astype(np.uint8), cv2.COLORMAP_OCEAN)
+        list.append(image)
+    return list
+
+def compare_sizes(size_video, size_fluo, fluo_list):
+    ret = []
+    for i in range(size_video):
+        n = int(i / (size_video/size_fluo))
+        ret.append(fluo_list[n])
+    return ret
+
+def overlap_2(video_data, fluo_data):
+    overlapped = []
+    for i in range(1800):
+    #for i in range(N_iterations):
+        image = cv2.addWeighted(video_data[i], 1, fluo_data[i], 0.7, 0)
+        overlapped.append(image)
+    return overlapped
+
+def find_contour(img, lower, upper):
+    # create NumPy arrays from the boundaries
+    lower = np.array(lower, dtype="uint8")
+    upper = np.array(upper, dtype="uint8")
+
+    # # find the colors within the specified boundaries and apply
+    # # the mask
+    mask = cv2.inRange(img, lower, upper)
+    output = cv2.bitwise_and(img, img, mask=mask)
+
+    ret, thresh = cv2.threshold(mask, 40, 255, 0)
+    if (int(cv2.__version__[0]) > 3):
+        contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    else:
+        im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+    if len(contours) != 0:
+        # draw in blue the contours that were founded
+        cv2.drawContours(output, contours, -1, 255, 3)
+
+        # find the biggest countour (c) by the area
+        c = max(contours, key=cv2.contourArea)
+        x, y, w, h = cv2.boundingRect(c)
+
+        # draw the biggest contour (c) in green
+        cv2.rectangle(output, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        return x + 0.5 * w, y + 0.5 * h
+    return 0, 0
+
+
+def calculate_transform_matrix(source, destination):
+    src_middle = find_contour(source, [190, 160, 70], [210, 190, 130])  # fluo
+    dst_middle = find_contour(destination, [20, 50, 80], [70, 140, 230])  # video
+    if src_middle == (0, 0) or dst_middle == (0, 0):
+        return np.float32([[1, 0, 0], [0, 1, 0]])
+    x = dst_middle[0] - src_middle[0]
+    y = dst_middle[1] - src_middle[1]
+    return np.float32([[1, 0, x], [0, 1, y]])
