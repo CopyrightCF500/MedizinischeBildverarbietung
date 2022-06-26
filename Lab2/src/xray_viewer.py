ddcc170779dc3016f26a84bfc35f280a8d862005
Alexander Straube <straube@hm.edu>
2022-06-26 14:35:40 +0200

    Lab2 Task 3

--- Lab2/src/xray_viewer.py
+++ Lab2/src/xray_viewer.py
@@ -1,23 +1,29 @@
 #!/usr/bin/python3
 
+import SimpleITK as sitk
+import itkwidgets
 import xray_util
 import typing
-import pydicom as dicom
-
+import pydicom
+import numpy as np
+import matplotlib
+import scipy
+import plotly
 
 class XRayViewer:
 
     def __init__(self, folder_path: str):
         self.dicom_files_folder_path = folder_path
         # List of all DICOM images
-        self.dicom_files_list: typing.List[dicom.FileDataset] = []
+        self.dicom_files_list: typing.List[pydicom.FileDataset] = []
         self.dicom_files_list = xray_util.get_dicom_list(folder_path)
-        self.dicom_images = xray_util.get_images(self.dicom_files_list)
+        self.dicom_images: typing.List[np.ndarray] = xray_util.get_images(self.dicom_files_list)
 
     def run(self):
 
         #print(type(self.dicom_files_list[0]))
-        print(xray_util.display_slice(self.dicom_images[150]))
+        #print(xray_util.display_slice(self.dicom_images[83]))
+        self.display_slice()
         #xray_util.display_wireframe(self.dicom_files_list)
         #print(xray_util.print_header(self.dicom_files_list[150].)))
         # show one slice
